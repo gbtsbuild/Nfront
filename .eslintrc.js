1c183c1a42a231f03a483a9c46b4e9a2b02457d3
Victor <victor@gbtsbuildingmaterials.com>
2025-06-04 21:47:07 +0000

    Upgrade ESLint and plugins

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+{ 
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:jsx-a11y/recommended",
+    ],
+    "plugins": ["react", "react-hooks", "jsx-a11y"],
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "rules": {},
+}
