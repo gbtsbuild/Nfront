b88400dc0562d666a860185805b04beee76c0fce
Victor <victor@gbtsbuildingmaterials.com>
2025-06-11 09:07:23 +0000

    moved HomePage.css into .src/style/HomePage.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./HomePage.css";
+import "../HomePage.css";
 
 // Main pages
 import HomePage from "./HomePage.js";
