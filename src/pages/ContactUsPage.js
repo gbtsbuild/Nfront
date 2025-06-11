b7bb5ae9239e2236c8d32cb595cfb379e625080f
Victor <victor@gbtsbuildingmaterials.com>
2025-06-11 15:34:13 +0100

    Update ContactUsPage.js
    Signed-off-by: Victor <victor@gbtsbuildingmaterials.com>

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.css"; // Ensure this file exists and contains your styles
 
 function ContactUsPage() {
   return (
