f5aaf8550072f8340bd7a60bf40c71c7eb183679
Victor <victor@gbtsbuildingmaterials.com>
2025-06-11 15:22:52 +0100

    Update PrivacyPolicyPage.js
    Signed-off-by: Victor <victor@gbtsbuildingmaterials.com>

--- src/pages/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicyPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./HomePage.css"; // Adjust if your CSS is elsewhere
+import "../styles/HomePage.css"; // Adjust if your CSS is elsewhere
 
 function PrivacyPolicyPage() {
   return (
